42689a9956c7792764cd8f27cf821463b2d5b59c
samik <s-samik@inbox.ru>
2025-06-19 16:40:41 +0300

    [Update]: Обновлены зависимости в package.json и исправлены отступы в конфигурации Nuxt

---
+++ frontend/pages/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import Chart from 'chart.js/auto';
+
+const chartData = ref([]);
+const selectedPeriod = ref('day');
+const customFrom = ref('');
+const customTo = ref('');
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
