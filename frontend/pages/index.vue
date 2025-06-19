fb37e748a089716a57637eecba76e8684319036f
samik <s-samik@inbox.ru>
2025-06-19 16:42:01 +0300

    [Add]: Добавлена функция для получения цен с учетом пользовательского периода

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,19 +1,29 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import axios from 'axios';
-import Chart from 'chart.js/auto';
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+import Chart from 'chart.js/auto'
 
-const chartData = ref([]);
-const selectedPeriod = ref('day');
-const customFrom = ref('');
-const customTo = ref('');
+const chartData = ref([])
+const selectedPeriod = ref('day')
+const customFrom = ref('')
+const customTo = ref('')
+
+const fetchPrices = async () => {
+   let url = 'http://localhost:3001/prices'
+
+   if (selectedPeriod.value === 'custom' && customFrom.value && customTo.value) {
+      url += `?from=${customFrom.value}&to=${customTo.value}`
+   }
+
+   const res = await axios.get(url)
+   chartData.value = res.data
+
+   renderChart()
+};
+
 
 </script>
 
-<template>
+<template></template>
 
-</template>
-
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
